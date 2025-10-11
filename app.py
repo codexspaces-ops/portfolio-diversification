9cc53220a10afa9958cdf69527ba3be293f3b81f
Rajiv Reddy <rajivreddyyedula22@gmail.com>
2025-10-11 17:56:41 +0530

    basic Templates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect,url_for,flash,jsonify  
 from app.auth.login import login_bp
 from app.auth.signup import signup_bp
 from app.config import users_collection
 import secrets
 # Create a Flask instance
-app = Flask(__name__)
+app = Flask(__name__, template_folder="app/templates")
 app.secret_key = secrets.token_hex(10)
 
 app.register_blueprint(login_bp, url_prefix="/auth", url_defaults={'users_collection': users_collection})
@@ -13,7 +13,7 @@ app.register_blueprint(signup_bp, url_prefix="/auth", url_defaults={'users_colle
 # Define a route for the home page
 @app.route('/')
 def home_page():
-    return "Welcome to the Portfolio Diversification App!"
+    return render_template('index.html')
 
 # Run the app
 if __name__ == '__main__':
