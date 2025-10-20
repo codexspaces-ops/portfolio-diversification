1f0a0e53dfdf205974fe05dc83314a9486bee10d
Aditya JP <adityajai243@gmail.com>
2025-10-20 13:15:11 +0530

    Frontend with landing page

--- app/static/js/navbar.js
+++ app/static/js/navbar.js
@@ -6,3 +6,18 @@ document.addEventListener('DOMContentLoaded', function () {
     sidebar.classList.toggle('active');
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const brokerSelect = document.getElementById("broker-select");
+    const instructionDivs = document.querySelectorAll(".broker-instructions");
+
+    brokerSelect.addEventListener("change", function () {
+        // Hide all instruction boxes
+        instructionDivs.forEach(div => div.style.display = "none");
+
+        // Show only selected broker instructions
+        const selected = this.value;
+        const activeDiv = document.getElementById(`${selected}-instructions`);
+        if (activeDiv) activeDiv.style.display = "block";
+    });
+});
